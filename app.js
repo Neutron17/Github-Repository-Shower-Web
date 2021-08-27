36a8270c04db6301f1555e83e9bc9ab5f2a1cad8
Neutron17 <sandormatyas17@gmail.com>
2021-08-27 22:31:50 +0200

    replaced jquery stuff with fetch

--- app.js
+++ app.js
@@ -5,37 +5,47 @@ let usrInp = document.getElementById("usr");
 let root = document.body;
 let arr = [];
 let prevOwn = "Neutron17";
-function foo(owner) {
-    if(owner != prevOwn) {
-        projList.innerHTML = "";
-        prevOwn = owner;
+async function foo(owner) {
+  if (owner != prevOwn) {
+    projList.innerHTML = "";
+    prevOwn = owner;
+  }
+  let x = await fetch("//api.github.com/users/" + owner + "/repos");
+  let data = await x.json();
+  let done = false;
+  for (i of JSON.parse(JSON.stringify(data))) {
+    if (!done) {
+      owner = i.owner.login;
+      user.innerHTML += "<i>" + owner + "</i>";
+      done = true;
     }
-    $.getJSON('//api.github.com/users/'+owner+'/repos',{},function(data){
-        //console.log(owner);
-        //console.log(data);
-        let done = false;
-        for(i of JSON.parse(JSON.stringify(data))) {
-            if(!done) {
-                owner = i.owner.login;
-                user.innerHTML += "<i>"+owner+"</i>";
-                done = true;
-            }
-            if(i.fork) continue;
-            console.log(i.name);
-            if(i.description == null) i.description = "";
-            let str = "<project-prototype name=\""+i.name+"\" desc=\""+i.description+"\" url=\""+i.html_url+"\" lang=\""+i.language+"\"";
-            if(i.archived == true) str += "Archived";
-            if(i.disabled == true) str += "Disabled";
-            str += "></project-prototype>";
-            projList.innerHTML += str;
-        }
-    });
+    if (i.fork) continue;
+    console.log(i.name);
+    if (i.description == null) i.description = "";
+    let str =
+      '<project-prototype name="' +
+      i.name +
+      '" desc="' +
+      i.description +
+      '" url="' +
+      i.html_url +
+      '" lang="' +
+      i.language +
+      '"';
+    if (i.archived == true) str += "Archived";
+    if (i.disabled == true) str += "Disabled";
+    str += "></project-prototype>";
+    projList.innerHTML += str;
+  }
 }
 function submit() {
-    //console.log("submit "+usrInp.value+" "+user.innerHTML); // Unsafe, no XSS protection
-    user.innerHTML = user.innerHTML.replace(user.innerHTML.split(" ")[user.innerHTML.split(" ").length-1], usrInp.value);
-    let owner = user.innerHTML.split(" ")[user.innerHTML.split(" ").length-1];
-    //console.log("own: "+owner)
-    foo(owner);
-    //user.value = user.innerHTML.replace(user.innerHTML.split(" ")[user.innerHTML.split(" ").length  ], usrInp.value)
+  //console.log("submit "+usrInp.value+" "+user.innerHTML); // Unsafe, no XSS protection
+  user.innerHTML = user.innerHTML.replace(
+    user.innerHTML.split(" ")[user.innerHTML.split(" ").length - 1],
+    usrInp.value
+  );
+  let owner = user.innerHTML.split(" ")[user.innerHTML.split(" ").length - 1];
+  //console.log("own: "+owner)
+  foo(owner);
+  //user.value = user.innerHTML.replace(user.innerHTML.split(" ")[user.innerHTML.split(" ").length  ], usrInp.value)
 }
